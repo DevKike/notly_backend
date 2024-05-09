5310180e144bd6610b27571d5b38bccf78a185fd
DevKike <alvaronarvaez151@gmail.com>
2024-05-09 00:07:36 -0500

    Init files were created

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const config = require("./config/config");
+
+const app = express();
+const port = config.SERVER.PORT;
+
+app.listen(port);
+
+try {
+    console.log(`Server running on port ${port}`);
+} catch (error) {
+    throw error;
+}
