669ed9e4d417cc52d003c6d50abc537c375b7967
DevKike <alvaronarvaez151@gmail.com>
2024-05-09 00:11:37 -0500

    Minor things were changed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const config = require('./config/config');
+const express = require("express");
+const config = require("./config/config");
 
 const app = express();
 const port = config.SERVER.PORT;
@@ -7,7 +7,7 @@ const port = config.SERVER.PORT;
 app.listen(port);
 
 try {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 } catch (error) {
-    throw error;
+  throw error;
 }
