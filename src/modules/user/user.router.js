6b54f05334de7ba5579ab340f6edf95020799a2f
DevKike <alvaronarvaez151@gmail.com>
2024-05-13 04:01:58 -0500

    An validation was added when role was not found

--- src/modules/user/user.router.js
+++ src/modules/user/user.router.js
@@ -1,21 +1,30 @@
 const express = require("express");
 const registerUser = require("./controller/user.controller");
+const schemaValidator = require("../../middleware/schemaValidator.middleware");
+const registerSchema = require("./schema/user.schema");
 
 const userRouter = express.Router();
 
-userRouter.post("/", async (req, res) => {
+userRouter.post("/", schemaValidator(registerSchema), async (req, res) => {
   try {
     const userData = req.body;
+    console.log(userData);
     await registerUser(userData);
 
     res.status(201).json({
       message: "User was succesfully registered",
     });
   } catch (error) {
-    res.status(500).json({
-      message: "User was not created",
-      error: error.message,
-    });
+    if (error.message === "Email already in use" || error.message === "Role was not found") {
+      res.status(400).json({
+        error: error.message,
+      });
+    } else {
+      res.status(500).json({
+        message: "User was not created",
+        error: error.message,
+      });
+    }
   }
 });
 
