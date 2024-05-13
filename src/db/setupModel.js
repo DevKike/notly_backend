a8a3b86ca8eaea494de6619c4177457dd9ed39bd
DevKike <alvaronarvaez151@gmail.com>
2024-05-13 02:39:02 -0500

    Inits for user and role models were added

--- src/db/setupModel.js
+++ src/db/setupModel.js
@@ -1,7 +1,11 @@
-const { Company, CompanySchema } = require("../modules/company/model/company.model")
+const { Company, CompanySchema } = require("../modules/company/model/company.model");
+const { Role, RoleSchema } = require("../modules/user/model/role/role.model");
+const { User, UserSchema } = require("../modules/user/model/user/user.model");
 
 const setupModel = (sequelize) => {
-    Company.init(CompanySchema, Company.config(sequelize));
-}
+  Company.init(CompanySchema, Company.config(sequelize));
+  User.init(UserSchema, User.config(sequelize));
+  Role.init(RoleSchema, Role.config(sequelize));
+};
 
 module.exports = setupModel;
