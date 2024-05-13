25aa5d2597ed9d4fda7caad661ffbe7e616a235e
DevKike <alvaronarvaez151@gmail.com>
2024-05-13 04:02:29 -0500

    Worked on user logic using Joi

--- src/modules/user/controller/user.controller.js
+++ src/modules/user/controller/user.controller.js
@@ -1,17 +1,26 @@
-const { findRoleByName, register } = require("../service/user.service");
+const { findUserByEmail, findRoleByName, register } = require("../service/user.service");
 
 const registerUser = async (userData) => {
   try {
+    const isEmailExists = await findUserByEmail(userData);
+
+    if (isEmailExists) {
+      throw new Error("Email already in use");
+    }
+
     const role = await findRoleByName(userData);
 
     if (!role) {
       throw new Error("Role was not found");
     }
-    
+
     const roleId = role.dataValues.id;
 
-    const newUserData = ({ ...userData, roleId });
-    delete newUserData.role;
+    const newUserData = { ...userData, roleId };
+
+    if (newUserData.role) {
+      delete newUserData.role;
+    }
 
     return await register(newUserData);
   } catch (error) {
