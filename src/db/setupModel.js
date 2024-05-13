86ed5b1e4af93d6a6a55cd5599be62afec3ae50e
DevKike <alvaronarvaez151@gmail.com>
2024-05-13 02:40:29 -0500

    Minor things were changed

--- src/db/setupModel.js
+++ src/db/setupModel.js
@@ -1,6 +1,6 @@
 const { Company, CompanySchema } = require("../modules/company/model/company.model");
-const { Role, RoleSchema } = require("../modules/user/model/role/role.model");
-const { User, UserSchema } = require("../modules/user/model/user/user.model");
+const { Role, RoleSchema } = require("../modules/user/model/role.model");
+const { User, UserSchema } = require("../modules/user/model/user.model");
 
 const setupModel = (sequelize) => {
   Company.init(CompanySchema, Company.config(sequelize));
