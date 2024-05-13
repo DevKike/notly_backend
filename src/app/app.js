29334a9ff9cafbe3903fb39de0ddab80175fc1fd
DevKike <alvaronarvaez151@gmail.com>
2024-05-13 02:38:09 -0500

    user router was added

--- src/app/app.js
+++ src/app/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const companyRouter = require("../modules/company.router");
+const companyRouter = require("../modules/company/company.router");
+const userRouter = require("../modules/user/user.router");
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/company", companyRouter);
+app.use("/user", userRouter)
 
 module.exports = app;
