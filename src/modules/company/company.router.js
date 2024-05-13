7bf4a2a06dce4661033177d4ce83716027760bef
DevKike <alvaronarvaez151@gmail.com>
2024-05-13 02:39:36 -0500

    Minor things were changed

--- src/modules/company/company.router.js
+++ src/modules/company/company.router.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const registerCompany = require("./company/controller/company.controller");
+const registerCompany = require("./controller/company.controller");
 
 const companyRouter = express.Router();
 
