a6b66d58912876233de2ded66478fd00111b9bbd
DevKike <alvaronarvaez151@gmail.com>
2024-05-17 03:23:02 -0500

    Project router was added to app

--- src/app/app.js
+++ src/app/app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const companyRouter = require("../modules/company/company.router");
 const employeeRouter = require("../modules/employee/employee.router");
+const projectRouter = require("../modules/project/project.router");
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/company", companyRouter);
-app.use("/employee", employeeRouter)
+app.use("/employee", employeeRouter);
+app.use("/project", projectRouter);
 
 module.exports = app;
