b676f8ec69ae6eefdc87fab41bf132a455691560
DevKike <alvaronarvaez151@gmail.com>
2024-05-17 21:46:43 -0500

    http and cors were added

--- src/app/app.js
+++ src/app/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const companyRouter = require("../modules/company/company.router");
 const employeeRouter = require("../modules/employee/employee.router");
 const projectRouter = require("../modules/project/project.router");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/company", companyRouter);
